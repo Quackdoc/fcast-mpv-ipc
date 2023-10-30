bce4aa4ed249a2ab39673b9beb725dd7e1bc21ab
QuackDoc <quackdoctech@gmail.com>
2023-10-29 23:03:36 -0400

    create header, still doesn't work though

--- src/fcastmanager.rs
+++ src/fcastmanager.rs
@@ -89,101 +89,39 @@ fn listen2(mut stream: TcpStream) {
 
         println!("crafting response");
         let response = update_state2();
-        //let response_json = serde_json::to_string(&response).unwrap();
-        let response_txt = format!("{{ time: {}, state: {} }}", response.time, response.state );
+        let response_json = serde_json::to_string(&response).unwrap();
+        //let response_txt = format!("{{ time: {}, state: {} }}", response.time, response.state );
         //let response_txt = "{ time: {}, state: {} }";
         //println!("\{ time: {}, state: {} \}", response.time, response.state );
-        println!("sending json data: {}", response_txt);
-        let data_respond = response_txt.as_bytes();
-        stream.write(data_respond).unwrap();
+        println!("sending json data: {}", response_json);
+        let msg_respond = response_json.as_bytes();
+        let data_respond = craft_data(msg_respond, Opcode::PlaybackUpdate);
+        println!("responding with the raw data {:?}", data_respond);
+        stream.write(&data_respond).unwrap();
         thread::sleep(time::Duration::from_millis(250));
         println!("send the data");
     }
 
 }
 
-//fn listener(mut stream: TcpStream) {
-//    let mut data = [0 as u8; MAXIMUM_PACKET_LENGTH]; // using 32k buffer
-//    //let mut buf = vec![];
-//    //let mut header = [0 as u8; 5];
-//    //let mut pull_size: u8 = 0;
-//    let mut combined_data: Vec<u8> = vec![];
-//    //TODO: grayjay first packet is the length header, second packet has opcode + body
-//    while match stream.read(&mut data) {
-//        Ok(size) => {
-//            println!("\n");
-//            if size == 4 {
-//                //println!("data size should be: {:?}", &data[0..4]);
-//                //let bytes = &data[0..4];
-//                //pull_size = u32::from_ne_bytes(bytes.try_into().unwrap()) as u8;
-//                combined_data.extend_from_slice(&data[0..4]);
-//                true
-//            } else {
-//                combined_data.extend_from_slice(&data[0..size]);
-//                //println!("data usize is: {}", size);
-//                //println!("data: {:?}", &combined_data[0..size + 4]);
-//                let header_info = read_header(&combined_data[0..5]);
-//                //println!("opcode: {:?}", &header_info.opcode);
-//                
-//                if header_info.size == 0 {
-//                    panic!("size should never be zero");
-//                } else {
-//                    println!("header info size: {:?}", &header_info.size);
-//                }
-//                match header_info.opcode {
-//                    Opcode::None => println!("No Body, Opcode is {:?}", header_info.opcode),
-//                    Opcode::Play => {
-//                        thread::spawn(move|| {
-//                            play(read_body(&combined_data, header_info.size))});  
-//                        },
-//                    Opcode::Pause => {
-//                            thread::spawn(move|| {pause()});
-//                        },
-//                    Opcode::Resume => {
-//                            thread::spawn(move|| {resume()});
-//                        },
-//                    Opcode::Stop => {
-//                            thread::spawn(move|| {stop()});
-//                            },
-//                    Opcode::Seek => {
-//                            thread::spawn(move|| { seek(read_body(&combined_data, header_info.size))});
-//                            },
-//                    Opcode::PlaybackUpdate => panic!("Incomming stream should never be {:?}", header_info.opcode),
-//                    Opcode::VolumeUpdate => panic!("Incomming stream should never be {:?}", header_info.opcode),
-//                    Opcode::SetVolume => {
-//                            thread::spawn(move|| {setvol(read_body(&combined_data, header_info.size))});
-//                            },
-//                }
-//                let response = update_state2();
-//                let response_json = serde_json::to_string(&response).unwrap();
-//                //println!("json is: {}", response_json);
-//                let data_respond = response_json.as_bytes();
-//                stream.write(data_respond).unwrap();
-//                thread::sleep(time::Duration::from_millis(250));
-//                //println!("\n");
-//                //data.clear();
-//                stream.flush().unwrap();
-//                //combined_data.clear();
-//                combined_data = vec![];
-//                false
-//            }
-//        },
-//        Err(_) => {
-//             println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-//             stream.shutdown(Shutdown::Both).unwrap();
-//             false
-//        }
-//    } {}
-//}
+fn craft_data(message: &[u8], opcode: Opcode) -> Vec<u8> {
+
+    let message_size = message.len() as u32 + 1;
+    let op: u8 = opcode as u8;
+    let size_bytes = message_size.to_ne_bytes();
+    let mut header_bytes: Vec<u8> = vec![];
+    header_bytes.extend_from_slice(&size_bytes);
+    header_bytes.push(op);
+    header_bytes.extend_from_slice(&message);
+    //println!("header bytes are {:?}", header_bytes);
+    return header_bytes;
+}
 
 fn read_header(data: &[u8]) -> HEADER {
 
     let header_size_bytes = &data[0..4];
     let header_size = u32::from_ne_bytes(header_size_bytes.try_into().unwrap());
     let header_op = data[4];
-
-    println!("opcode is {}", &header_op);
-    println!("header_size is {}", &header_size);
     
     return HEADER::new(header_size, Opcode::from_u8(header_op));
 }
